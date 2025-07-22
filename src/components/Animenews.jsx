958f0603a1393b52a80281130654b8f925ecd59a
Subomi Obans <subomi1@users.noreply.github.com>
2025-07-22 17:31:54 +0100

    Added a space to display some anime characters

--- src/components/Animenews.jsx
+++ src/components/Animenews.jsx
@@ -1,22 +1,63 @@
 import { useState, useEffect } from "react";
 
-export default function Animenews() {
-    const [news, setNews] = useState([]);
-    const [loading, setLoading] = useState(true);
-  
-    useEffect(() => {
-      fetch("https://api.jikan.moe/v4/anime/news") // 5114 = Fullmetal Alchemist: Brotherhood
-        .then((response) => response.json())
-        .then((data) => {
-          setNews(data.data);
-          setLoading(false);
-        })
-        .catch((error) => console.error("Error fetching data:", error));
-    }, []);
+export default function Animenews(isHome) {
+  const [characters, setCharacters] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
 
-    console.log(news);
+  useEffect(() => {
+    fetch(`https://api.jikan.moe/v4/characters?page=${currentPage}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setCharacters(data.data);
+        setLoading(false);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
 
-    return (
-        <></>
-    );
+  let cssClasses = "";
+  let topTen = "";
+  console.log(characters);
+  if (isHome) {
+    topTen = characters.splice(0, 10);
+    cssClasses = "h-[600px] overflow-y-hidden flex w-full items-center";
+  } else {
+    topTen = characters;
+    cssClasses =
+      "gap-[-100px] flex-wrap flex w-full items-center justify-between";
+  }
+
+  return (
+    <section className="p-5">
+      <h1 className="w-full text-xl text-white titleA pb-5">
+        Some Anime Characters
+      </h1>
+      <ul className={cssClasses}>
+        {topTen.map((anime) => (
+          <li key={anime.mal_id} className="card-hover">
+            <div className="glass h-auto w-[300px]">
+              <img src={anime.images.webp.image_url} alt="" className="h-1/2" />
+              <div className="text font-bold text-xs flex flex-col gap-3 pt-2">
+                <h1 className="flex flex-wrap bebes gap-1">
+                  Name: <span>{anime.name} </span>
+                </h1>
+                <h1 className="flex flex-wrap bebes gap-1">
+                  Name-kanji: <span>{anime.name_kanji} </span>
+                </h1>
+                <p className="raleway flex flex-wrap gap-2">
+                  Nicknames:{" "}
+                  {anime.nicknames.map((nick, index) => (
+                    <p key={index}>
+                      {nick}
+                      <br />
+                    </p>
+                  ))}
+                </p>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,11 +2,8 @@ import Navbar from "../components/NavBar";
 import Carousel from "../components/Carousel";
 import Hero from "../components/Hero";
 import TrendingAnime from "../components/Trendinganime";
-import UpcomingAnime from "../components/upcomingAnime";
 import Viewalltrendy from "../ViewAll/ViewAllTrendy";
-import Trendingmanga from "../components/Trendingmanga";
 import Animenews from "../components/Animenews";
-import RandomAnimeQuotes from "../components/RandomAnimeQuotes";
 export default function Homepage(){
     return (
         <>
@@ -15,10 +12,7 @@ export default function Homepage(){
           <Hero/>
           <TrendingAnime isHome/>
             <Viewalltrendy/>
-            {/* <UpcomingAnime/> */}
-            {/* <Trendingmanga isHome/> */}
-            <Animenews/>
-           <RandomAnimeQuotes/>
+            <Animenews isHome/>
         </>
     )
 }
